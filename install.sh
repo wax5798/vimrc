1070e994f0ea672629f4fc493cd925fb9d6fa9d8
wax5798 <810905061@qq.com>
2016-09-16 10:56:38 +0800

    说明YCM需要额外配置

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "安装将花费一定时间，请耐心等待直到安装完成^_^"
 mv -f ~/vim ~/vim_old
-cd ~/ && git clonet https://github.com/wax5798/vimrc.git
+cd ~/ && git clone https://github.com/wax5798/vimrc.git
 mv -f ~/.vim ~/.vim_old
 mv -f ~/vimrc ~/.vim
 mv -f ~/.vimrc ~/.vimrc_old
@@ -10,6 +10,7 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo "wax5798正在努力为您安装bundle程序" > wax5798
 echo "安装完毕将自动退出" >> wax5798
 echo "请耐心等待" >> wax5798
-vim wax5798 -c "BundleInstall" -c "q" -c "q"
+echo "YouCompleteMe需要额外配置：https://github.com/Valloric/YouCompleteMe" >> wax5798
+vim wax5798 -c "PluginInstall" -c "q" -c "q"
 rm wax5798
 echo "安装完成"
